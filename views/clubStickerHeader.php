92c2b4f07a99073f69040a1daf6e1d411cc3b452
Datrinon <dtrinh089@gmail.com>
2020-10-19 00:08:35 -0700

    Create Footer and Header
    Notice that href='.' means ./, the current directory.
    
    This is superior to # to get to the home page.

---
+++ views/clubStickerHeader.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7 "> <![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie10 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie10 lt-ie9"> <![endif]-->
+<!--[if IE 9]><html class="no-js lt-ie10"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+
+    <!-- Basic Page Needs -->
+    <meta charset="utf-8">
+    <title>Club Sticker</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="#">
+
+    <!-- FONTS -->
+    <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Roboto:100,300,400,400italic,700'>
+    <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Patua+One:100,300,400,400italic,700'>
+
+    <!-- CSS -->
+    <link rel='stylesheet' href='css/global.css'>
+    <link rel='stylesheet' href='css/structure.css'>
+    <link rel='stylesheet' id='style-static' href='css/be_style.css'>
+    <link rel='stylesheet' href='css/custom.css'>
+
+    <!-- CSS Skin-->
+    <link rel='stylesheet' href='css/skins/orange/stylec721.css'>
+
+
+</head>
+
+<body class="archive    -product with_aside aside_right woocommerce woocommerce-page color-blue layout-full-width header-modern subheader-title-left ">
+
+    <!-- Main Theme Wrapper -->
+    <div id="Wrapper">
+        <!-- Header Wrapper -->
+        <div id="Header_wrapper">
+            <!-- Header -->
+            <header id="Header">
+                <!-- Header Top -  Info Area -->
+                <div id="Action_bar">
+                </div>
+                <!-- Header -  Logo and Menu area -->
+                <div id="Top_bar">
+                    <div class="container">
+                        <div class="column one">
+                            <div class="top_bar_left clearfix">
+                                <!-- Logo-->
+                                <div class="logo">
+                                    <img class="scale-with-grid" src="club-sticker-images/logo.png" alt="Club Sticker" />
+                                </div>
+                                <!-- Main menu-->
+                                <div class="menu_wrapper">
+                                    <nav id="menu">
+                                        <ul id="menu-main-menu" class="menu">
+                                            <li id="menu-item-1354">
+                                                <a href="." target="_Self""><span>Sticker Catalog</span></a>
+                                            </li>
+                                            <li id=" menu-item-1354">
+                                                    <a href="." target="_self"><span>Upload Artwork</span></a>
+                                            </li>
+                                            <li id="login-button">
+                                                <a href="?ctlr=register&amp;action=register" target="_self" class="topnav-right" login><span>Login / Signup</span></a>
+                                            </li>
+                                    </nav><a class="responsive-menu-toggle" href="#"><i class="icon-menu"></i></a>
+                                </div>
+                                <!-- Banner area - only for certain pages-->
+                                <div class="banner_wrapper">
+                                    <img src="club-sticker-images/banner-noprice.png" alt="">
+                                </div>
+                                <!-- Header Searchform area-->
+                                <!-- Come back to this later and edit this-->
+                            </div>
+                            <div class="top_bar_right">
+                                <div class="top_bar_right_wrapper">
+                                    <!-- Shopping cart icon-->
+                                    <a id="header_cart" href="#"><i class="icon-basket"></i><span>0</span></a>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
--- views/guitarShopHeader.php
+++ views/guitarShopHeader.php
@@ -1,9 +1,10 @@
 <?php
-    $categoryDAM = new CategoryDAM();
-    $categories = $categoryDAM->readCategories();
+$categoryDAM = new CategoryDAM();
+$categories = $categoryDAM->readCategories();
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title><?php echo Page::$title; ?></title>
     <link rel="stylesheet" type="text/css" href="content/css/main.css">
@@ -14,29 +15,29 @@
         <h1>My Guitar Shop</h1>
     </header>
     <aside>
-    <!-- These links are for testing only.
+        <!-- These links are for testing only.
          Remove them from a production application. -->
-    <h2>Links</h2>
-    <nav>
-    <ul>
-        <li>
-            <a href=".">Home</a>
-        </li>
-        <li>
-            <a href="?ctlr=admin&amp;action=login">Admin</a>
-        </li>
-    </ul>
-    <h2>Categories</h2>
-    <ul>
-        <!-- display links for all categories -->
-        <?php foreach ($categories as $category) { ?>
-        <li>
-            <a href="?ctlr=home&amp;action=listProducts&amp;categoryId=<?php echo $category->id; ?>">
-                <?php echo $category->name; ?>
-            </a>
-        </li>
-        <?php } ?>
-        <li>&nbsp;</li>
-    </ul>
-    </nav>
-</aside>
+        <h2>Links</h2>
+        <nav>
+            <ul>
+                <li>
+                    <a href=".">Home</a>
+                </li>
+                <li>
+                    <a href="?ctlr=admin&amp;action=login">Admin</a>
+                </li>
+            </ul>
+            <h2>Categories</h2>
+            <ul>
+                <!-- display links for all categories -->
+                <?php foreach ($categories as $category) { ?>
+                    <li>
+                        <a href="?ctlr=home&amp;action=listProducts&amp;categoryId=<?php echo $category->id; ?>">
+                            <?php echo $category->name; ?>
+                        </a>
+                    </li>
+                <?php } ?>
+                <li>&nbsp;</li>
+            </ul>
+        </nav>
+    </aside>
